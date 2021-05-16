03413b0cf08b8aca495a8366393ce2e528b68ab7
irnak4t <ima.tak0610@gmail.com>
2021-05-16 20:48:03 +0900

    Update process to attach run command

--- init.lua
+++ init.lua
@@ -5,6 +5,11 @@ for _, name in ipairs(modules) do M[name] = require('gdscript.' .. name) end
 
 textadept.file_types.extensions.gd = 'gdscript'
 textadept.editing.comment_string = '#'
-textadept.run.run_commands.gdscript = M.run.build_run_command()
+
+events.connect(events.LEXER_LOADED, function(name)
+  if name == 'gdscript' then
+    textadept.run.run_commands.gdscript = M.run.build_run_command()
+  end
+end)
 
 return M
